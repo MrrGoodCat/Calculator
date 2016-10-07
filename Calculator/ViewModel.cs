4bce3a241598490824803f2ae9bfaa91037640c8
MrrGoodCat <mrrgoodcat@gmail.com>
2016-10-07 18:38:27 +0300

    Добавьте файлы проекта.

---
+++ Calculator/ViewModel.cs
@@ -0,0 +1,67 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Calculator
+{
+    public class ViewModel :  DependencyObject
+    {
+
+
+        public int Result
+        {
+            get { return (int)GetValue(ResultProperty); }
+            set { SetValue(ResultProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Result.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ResultProperty =
+            DependencyProperty.Register("Result", typeof(int), typeof(ViewModel), new PropertyMetadata(0));
+
+
+
+        private ICommand calculate;
+        private int result;
+
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public ICommand Calculate
+        {
+            get
+            {
+                return calculate ?? (calculate = new RelayCommand(() => 
+                {
+                    Result = FirstNumber + SecondNumber;
+                }));
+            }
+
+            set
+            {
+                calculate = value;
+            }
+        }
+
+        //public int Result
+        //{
+        //    get
+        //    {
+        //        return result;
+        //    }
+
+        //    set
+        //    {
+        //        result = value;
+        //        RaisePropertyChanged(() => Result);
+        //    }
+        //}
+
+
+    }
+}
